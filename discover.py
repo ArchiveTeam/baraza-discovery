e40bc67753492142e122a840d18a1707222755b5
Arkiver2 <Arkiver@hotmail.com>
2015-04-25 15:47:40 +0200

    discover.py: add support for 'labelfr'

--- discover.py
+++ discover.py
@@ -18,6 +18,7 @@ def main():
     # Normal programs use 'argparse' but this keeps things simple
     item_value = sys.argv[1]
     output_filename = sys.argv[2]  # this should be something like myfile.txt.gz
+    lang = sys.argv[3]
 
     print('Starting', item_value)
 
@@ -26,14 +27,14 @@ def main():
     num = 0
     
     while True:
-        for shortcode in check(item_value, num):
+        for shortcode in check(item_value, num, lang):
             # Write the valid result one per line to the file
             line = '{0}\n'.format(shortcode)
             gzip_file.write(line.encode('ascii'))
-        htmltext = requests.get('http://www.google.com/baraza/en/label?lid={0}&start={1}'.format(item_value, str(int(num)+20)), headers=DEFAULT_HEADERS).text
-        if not 'No questions related to this label.' in htmltext:
+        htmltext = requests.get('http://www.google.com/baraza/{2}/label?lid={0}&start={1}'.format(item_value, str(int(num)+20), lang), headers=DEFAULT_HEADERS).text
+        if not ('No questions related to this label.' in htmltext or 'Aucune question en rapport avec ce libell' in htmltext):
             num = num + 20
-        elif 'No questions related to this label.' in htmltext:
+        elif ('No questions related to this label.' in htmltext or 'Aucune question en rapport avec ce libell' in htmltext):
             break
 
     gzip_file.close()
@@ -41,8 +42,8 @@ def main():
     print('Done')
 
 
-def check(item_value, num):
-    url = 'http://www.google.com/baraza/en/label?lid={0}&start={1}'.format(item_value, num)
+def check(item_value, num, lang):
+    url = 'http://www.google.com/baraza/{2}/label?lid={0}&start={1}'.format(item_value, num, lang)
     counter = 0
     while True:
         # Try 4 times before giving up
@@ -58,11 +59,11 @@ def check(item_value, num):
             time.sleep(10)
         else:
             if text:
-                yield 'id:{0}'.format(item_value)
+                yield 'id{1}:{0}'.format(item_value, lang)
                 for thread in extract_threads(text):
-                    yield 'thread:{0}'.format(thread)
+                    yield 'thread{1}:{0}'.format(thread, lang)
                 for user in extract_users(text):
-                    yield 'user:{0}'.format(user)
+                    yield 'user{1}:{0}'.format(user, lang)
             break  # stop the while loop
 
         counter += 1
@@ -99,11 +100,11 @@ def fetch(url):
 
 def extract_threads(text):
     '''Return a list of tags from the text.'''
-    return re.findall(r'"\/baraza\/en\/thread\?tid=([a-z0-9]+)', text)
+    return re.findall(r'"\/baraza\/[a-z][a-z]\/thread\?tid=([a-z0-9]+)', text)
 
 def extract_users(text):
     '''Return a list of tags from the text.'''
-    return re.findall(r'"\/baraza\/en\/user\?userid=([0-9]+)', text)
+    return re.findall(r'"\/baraza\/[a-z][a-z]\/user\?userid=([0-9]+)', text)
 
 if __name__ == '__main__':
     main()
